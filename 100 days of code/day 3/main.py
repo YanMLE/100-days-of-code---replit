3173069579043c52ca97e3eef9c412e76dafb3b2
Yan Vinícius <158772389+YanMLE@users.noreply.github.com>
2024-03-07 00:14:47 -0300

    Add files via upload

---
+++ 100 days of code/day 3/main.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+lunch = input("What are you having for lunch? ")
+print(f"{name}, is going to be chowing down on, {lunch}, very soon!")
+# I already did something like this before on the 2nd day of the course 
